70afb83d2027360cb47c26304061a757027d7ec5
Debodipto Roy <debodiptoroy@gmail.com>
2019-05-09 18:00:10 +0530

    Add files via upload

---
+++ Pre-Requirements Check.bat
@@ -0,0 +1,15 @@
+color 0D
+cls
+@echo OFF
+echo Pre-requirement checks for Libraries :
+
+echo Installing ...
+
+pip install os
+pip install shutil
+pip install paramiko
+pip install logging
+pip install termcolor
+pip install scp
+
+cls
